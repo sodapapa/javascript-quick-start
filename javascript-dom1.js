4c8bbb0ae2662d7ccdd3005051e868fafb1e4651
sodapapa <415hdh@gmail.com>
2020-04-20 08:57:42 +0900

    스터디 실습

--- javascript-dom1.js
+++ javascript-dom1.js
@@ -1,3 +1,25 @@
+//  Ex2 : 엘리먼트 선택방법 개선하기
+window.addEventListener('load', function(){
+  var section2 = document.getElementById('section2');
+  var inputs = document.getElementsByTagName('input');
+
+  var txtY = inputs[0];
+  var txtY = inputs[1];
+  var btnAdd =inputs[2];
+  var txtResult = inputs[3];
+
+
+  btnAdd.onclick = function(){
+    console.log('add');
+    var x = parseInt(txtX.value);
+    var y = parseInt(txtY.value);
+
+    txtResult.value = x + y;
+  }
+})
+
+
+
 window.addEventListener('load', function(){
   var txtX = document.getElementById('txt-x');
   var txtY = document.getElementById('txt-y');
@@ -9,7 +31,7 @@ window.addEventListener('load', function(){
     console.log('add');
     var x = parseInt(txtX.value);
     var y = parseInt(txtY.value);
-      
+
     txtResult.value = x + y;
   }
 })
