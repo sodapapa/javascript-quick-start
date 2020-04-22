3e5150d138fca94549e9a073e316761404500220
sodapapa <415hdh@gmail.com>
2020-04-22 09:10:57 +0900

    아침 자바스크립트 스터디

--- javascript-dom1.js
+++ javascript-dom1.js
@@ -1,3 +1,72 @@
+//Ex10-클릭한 컬럼을 기준으로 레코드 정렬하기 #1
+window.addEventListener("load", function(){
+
+  var notices = [
+      {"id":1, "title":"유투브에 끌려다니지 않으려고 했는데....ㅜㅜ..", "regDate":"2019-02-05", "writerId":"newlec", "hit":2},
+      {"id":2, "title":"자바스크립트란..", "regDate":"2019-02-02", "writerId":"newlec", "hit":0},
+      {"id":3, "title":"기본기가 튼튼해야....", "regDate":"2019-02-01", "writerId":"newlec", "hit":1},
+      {"id":4, "title":"근데 조회수가 ㅜㅜ..", "regDate":"2019-01-25", "writerId":"newlec", "hit":0}
+  ];
+
+  var section = document.querySelector("#section10");
+  
+  var noticeList =section.querySelector(".notice-list");
+  var titldTd = section.querySelector(".title");
+  var tbodyNode = noticeList.querySelector("tbody");
+
+  var bindData = function(){
+      var template = section.querySelector("template");
+
+      for(var i=0; i<notices.length; i++){
+          var cloneNode = document.importNode(template.content, true);
+          var tds = cloneNode.querySelectorAll("td");
+          tds[0].textContent = notices[i].id;            
+
+          var aNode = tds[1].children[0];
+          aNode.href=notices[i].id;
+          aNode.textContent = notices[i].title;
+
+          tds[2].textContent = notices[i].regDate;
+          tds[3].textContent = notices[i].writerId;
+          tds[4].textContent = notices[i].hit;
+
+          tbodyNode.appendChild(cloneNode);
+      }
+  };
+
+  bindData();
+
+  var titleSorted = false;
+
+  titldTd.onclick = function(){
+    tbodyNode.innerHTML ="" ;
+
+    if(!titleSorted){
+      
+      titleSorted = true;
+      notices.sort(function(a, b){
+        if(a.title < b.title)
+          return -1
+        
+        else if(a.title > b.title)
+          return 1
+  
+        else
+          return 0
+      })
+      
+    }else{
+      
+      notices.reverse();
+    }
+      
+      bindData();
+    };
+
+
+});
+
+
 //Ex9-다중 노드선택 방법과 일괄삭제, 노드의 자리바꾸기
 window.addEventListener("load", function(){
 
@@ -24,11 +93,25 @@ window.addEventListener("load", function(){
 
     for(var i = 0; i < inputs.length; i++)
       inputs[i].parentElement.parentElement.remove();    
-          
+
   };
 
   swapButton.onclick = function(){
-              
+    var inputs = tbody.querySelectorAll("input[type='checkbox']:checked")
+       
+    if(inputs.length != 2){
+      alert('2개만 선택해야함');
+      return;   
+    }                   
+
+    var trs = [];
+    for(var i = 0; i < inputs.length; i++)
+      trs.push (inputs[i].parentElement.parentElement);
+
+    var cloneNode = trs[0].cloneNode(true);
+
+    trs[1].replaceWith(cloneNode);
+    trs[0].replaceWith(trs[1]);   
       
   };
 
---
+++ javascript2-event.js
@@ -0,0 +1,65 @@
+// Ex3-이벤트 버블링 멈추기
+window.addEventListener("load", function(){
+
+    var section = document.querySelector("#section3");
+    
+    var imgList = section.querySelector(".img-list"); 
+    var addButton = section.querySelector(".add-button");
+    var currentImg = section.querySelector(".current-img");
+    
+    imgList.onclick = function(e){
+        
+    };
+
+    addButton.onclick = function(e){
+        
+    };
+
+}); 
+
+
+
+
+
+//Ex2-이벤트 버블링을 이용해 사용자 이벤트 처리하기:event Bubbling
+window.addEventListener("load", function(){
+
+    var section = document.querySelector("#section2");
+    var imgList = section.querySelector(".img-list"); 
+    var currentImg = section.querySelector(".current-img");
+    
+    imgList.onclick = function(e){
+        
+    };
+}); 
+
+
+//연습문제 1-선택된 레코드 삭제하기:event target
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section1-1");
+    var buttons = section.querySelectorAll('input[type=button]')
+
+    for(var i = 0; i<buttons.length; i++)              
+    buttons[i].onclick = function(event){
+        event.target.parentElement.parentElement.remove();       
+        }          
+
+}); 
+
+
+
+//Ex1-선택된 이미지 보여주기:event target
+window.addEventListener("load", function(){
+
+    var section = document.querySelector("#section1");
+    
+    var imgs = section.querySelectorAll(".img");
+    var currentImg = section.querySelector(".current-img");
+    
+    for(var i = 0; i<imgs.length; i++)              
+        imgs[i].onclick = function(event){
+            currentImg.src=event.target.src;       
+
+        }
+        
+}); 
