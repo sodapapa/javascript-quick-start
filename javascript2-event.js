dc7607ac5148de7b3672988b3936ba42b4640ce2
sodapapa <415hdh@gmail.com>
2020-04-28 09:18:54 +0900

    자바스크립트 아침 스터디

--- javascript2-event.js
+++ javascript2-event.js
@@ -1,10 +1,120 @@
+
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section9");
+    var status = section.querySelector(".status");
+    var container = section.querySelector(".container");
+    var dragging = false;
+    var offset = {x:0, y:0};
+    var current = null;
+    var left = container.offsetLeft;
+    var top = container.offsetTop;
+    container.onmousedown = function(e){
+        if(e.target.classList.contains("box")){
+            dragging = true;
+            current = e.target;    
+            offset.x = e.offsetX;       
+            offset.y = e.offsetY;           
+
+        }                
+    };      
+    container.onmouseup = function(e){      
+        dragging = false;
+                         
+     };    
+
+    container.onmousemove = function(e){
+        if(!dragging) return;
+        dragging = true;                    
+
+        var x =  e.pageX-offset.x-left+"px";
+        var y = e.pageY-offset.y+-top+"px";                 
+        current.style.left = e.pageX-offset.x-left+"px";
+        current.style.top = e.pageY-offset.y+-top+"px";      
+                                    
+        status.innerText = "(x , y) " + x +", " + y;
+     };         
+
+ 
+});
+
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section8");
+    var box = section.querySelector(".box");
+    var container = section.querySelector(".container");
+    var dragging = false;
+    var offset = {x:0, y:0};
+    var current = null;
+
+    container.onmousedown = function(e){
+        if(e.target.classList.contains("box")){
+            dragging = true;
+            current = e.target;    
+            offset.x = e.offsetX;       
+            offset.y = e.offsetY;
+
+        }                
+    };      
+    container.onmouseup = function(e){      
+        dragging = false;
+                         
+     };    
+
+    container.onmousemove = function(e){
+        if(!dragging) return;
+        dragging = true;
+        current.style.left = e.x-offset.x+"px";
+        current.style.top = e.y-offset.y+"px";      
+
+     };         
+
+     box.onmousedown = function(e){
+
+     }
+});
+
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section7");
+    var box = section.querySelector(".box");
+    var container = section.querySelector(".container");
+    var dragging = false;
+    var offset = {x:0, y:0};
+    
+    container.onmousedown = function(e){
+        if(e.target === box)
+            dragging = true;
+                        
+    };      
+    container.onmouseup = function(e){      
+        dragging = false;
+                         
+     };    
+
+    container.onmousemove = function(e){
+        if(!dragging) return;
+        dragging = true;
+        box.style.left = e.x-offset.x+"px";
+        box.style.top = e.y-offset.y+"px";      
+
+     }; 
+
+     box.onmousedown = function(e){
+        offset.x = e.offsetX;
+        offset.y = e.offsetY;
+     }
+});
+
+
 window.addEventListener("load", function(){
     var section = document.querySelector("#section6");
     var box = section.querySelector(".box");
     var container = section.querySelector(".container");
 
     container.onclick = function(e){
+        console.log('_____________________________')
        console.log("e.x, e.y "+ e.x +  ","+ e.y);
+       console.log("client e.x, e.y "+ e.clientX +  ","+ e.clientY);
+       console.log("page e.x, e.y "+ e.pageX +  ","+ e.pageY);
+       console.log("offset e.x, e.y "+ e.offsetX +  ","+ e.offsetY);
 
        box.style.position = "absolute";
        box.style.left = e.x+"px";
